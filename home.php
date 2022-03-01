fbc279cfb4d44656f053e1c7a67fb1adeab076e0
VittorioDL <victor03.VDL@gmail.com>
2022-03-01 11:55:54 +0100

    Aggiunta pagina ricerca-reperti funzionante
    Aggiunta pulsante info e pulsante per tornare alla home dal risultato della ricerca, Modifiche varie nei select della ricerca

--- home.php
+++ home.php
@@ -4,6 +4,7 @@ include 'connect.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>CRUD TechSeum</title>
     <meta charset="UTF-8">
@@ -11,17 +12,18 @@ include 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <!-- Titolo -->
     <div class="container">
         <h2 class="text-center">Tech-Seum</h2>
-        <h3 class="text-center">CRUD</h4>    
+        <h3 class="text-center">CRUD</h4>
     </div>
-    
-    
+
+
     <!-- Bottone aggiunta reperti -->
     <div class="container">
-        <button class="btn btn-success my-5"> <a href="#" class="text-light text-decoration-none">Aggiungi reperto</a></button>
+        <button class="btn btn-success my-5"> <a href="inserimento-reperto.php" class="text-light text-decoration-none">Aggiungi reperto</a></button>
     </div>
 
     <!-- Barra di ricerca -->
@@ -29,7 +31,7 @@ include 'connect.php';
         <div class="container">
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
-                <button class="btn btn-info" type="button" id="button-addon1">Cerca</button>
+                    <button class="btn btn-primary" type="submit" id="button-addon1">Cerca</button>
                 </div>
                 <input name="nome" type="text" class="form-control" placeholder="Nome reperto">
                 <input name="anno_inizio" type="number" class="form-control" min="1800" step="1" placeholder="Anno inizio uso">
@@ -37,20 +39,19 @@ include 'connect.php';
 
                 <!-- Selezione autore -->
                 <select name="autore" class="form-control">
-                    <option value="">Autore</option>
+                    <option value="" class="text-white bg-secondary">Autore</option>
                     <?php
-                    $queryAutori = "select * from autore";
-                    $result1 = mysqli_query($conn, $queryAutori);
-                    while($row = mysqli_fetch_array($result1))
-                    {
-                        echo '<option value='.$row['codautore'].'>'.$row['nomeautore'].'</option>';
-                    }
+                        $queryAutori = "select * from autore order by nomeautore asc";
+                        $result1 = mysqli_query($conn, $queryAutori);
+                        while ($row = mysqli_fetch_array($result1)) {
+                            echo '<option value=' . $row['codautore'] . '>' . $row['nomeautore'] . '</option>';
+                        }
                     ?>
                 </select>
 
                 <!-- Selezione sezione -->
                 <select name="sezione" class="form-control">
-                    <option value="">Sezione</option>
+                    <option value="" class="text-white bg-secondary">Sezione</option>
                     <option value="I">Informatica</option>
                     <option value="E">Elettronica</option>
                     <option value="M">Meccanica</option>
@@ -59,7 +60,7 @@ include 'connect.php';
 
                 <!-- Selezione stato -->
                 <select name="stato" class="form-control">
-                    <option value="">Stato</option>
+                    <option value="" class="text-white bg-secondary">Stato</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -69,26 +70,24 @@ include 'connect.php';
 
                 <!-- Selezione materiale -->
                 <select name="materiale" class="form-control">
-                    <option value="">Materiale</option>
+                    <option value="" class="text-white bg-secondary">Materiale</option>
                     <?php
                     $queryMateriale = "select nomemateriale from compostoda";
                     $result2 = mysqli_query($conn, $queryMateriale);
                     $materiali = array();
-                    while($row = mysqli_fetch_array($result2))
-                    {   
+                    while ($row = mysqli_fetch_array($result2)) {
                         array_push($materiali, $row['nomemateriale']);
                     }
                     $materiali_unici = array_unique($materiali);
-                    foreach($materiali_unici as $m)
-                    {
-                        echo '<option value='.$m.'>'.$m.'</option>';
+                    foreach ($materiali_unici as $m) {
+                        echo '<option value=' . $m . '>' . $m . '</option>';
                     }
                     ?>
                 </select>
 
                 <!-- Selezione acquisizione -->
                 <select name="acquisizione" class="form-control">
-                    <option value="">Acquisizione</option>
+                    <option value="" class="text-white bg-secondary">Acquisizione</option>
                     <option value="D">Donazione</option>
                     <option value="A">Acquisto</option>
                     <option value="R">Rubato</option>
@@ -105,43 +104,59 @@ include 'connect.php';
     <div class="container">
         <table class="table text-center">
             <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome reperto</th>
-                <th scope="col">Sezione</th>
-                <th scope="col">Data catalogazione</th>
-                <th scope="col">Stato</th>
-                <th scope="col">Azioni</th>
-              </tr>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome reperto</th>
+                    <th scope="col">Sezione</th>
+                    <th scope="col">Data catalogazione</th>
+                    <th scope="col">Stato</th>
+                    <th scope="col">Azioni</th>
+                </tr>
             </thead>
             <tbody>
-            <?php
+                <?php
                 $query = "select * from repertinuova";
                 $result = mysqli_query($conn, $query);
-                if($result){
-                    while($row = mysqli_fetch_assoc($result)){
+                if ($result) {
+                    while ($row = mysqli_fetch_assoc($result)) {
                         $id = $row['codassoluto'];
                         $nome = $row['nome'];
                         $sezione = $row['sezione'];
                         $data = $row['datacatalogazione'];
                         $stato = $row['stato'];
 
+                        switch ($sezione) {
+                            case "E":
+                                $sezione = "Elettronica";
+                                break;
+                            case "I":
+                                $sezione = "Informatica";
+                                break;
+                            case "M":
+                                $sezione = "Meccanica";
+                                break;
+                            case "S":
+                                $sezione = "Scienze";
+                                break;
+                        }
+
                         echo '<tr>
-                        <th scope="row">'.$id.'</th>
-                        <td>'.$nome.'</td>
-                        <td>'.$sezione.'</td>
-                        <td>'.$data.'</td>
-                        <td>'.$stato.'</td>
+                        <th scope="row">' . $id . '</th>
+                        <td>' . $nome . '</td>
+                        <td>' . $sezione . '</td>
+                        <td>' . $data . '</td>
+                        <td>' . $stato . '</td>
                         <td>
-                            <button class="btn btn-primary"><a href="modifica-reperto.php?id='.$id.' " class="text-light text-decoration-none">Modifica</a></button>
-                            <button class="btn btn-danger"><a href="cancella-reperto.php?id='.$id.' " class="text-light text-decoration-none">Cancella</a></button>
-                        </td>
+                            <button class="btn btn-primary"><a href="modifica-reperto.php?id=' . $id . ' " class="text-light text-decoration-none">Modifica</a></button>
+                            <button class="btn btn-danger")"><a href="cancella-reperto.php?id=' . $id . ' " class="text-light text-decoration-none">Cancella</a></button>
+                            <button class="btn btn-info" ><a href="visualizza-reperto.php?id=' . $id . ' " class="text-light text-decoration-none">Info</a></button>
+                            </td>
                         </tr>';
                     }
                 }
-            ?>
+                ?>
             </tbody>
-          </table>
+        </table>
     </div>
 </body>
 </html>
---
+++ ricerca-reperti.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Risultato ricerca reperti</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Pulsante per tornare alla home -->
+    <div class="container"> 
+        <button class="btn btn-primary my-5"> <a href="home.php" class="text-light text-decoration-none">Home</a></button>
+    </div>
+
+    <!-- Risultato ricerca -->
+    <div class="container">
+        <table class="table text-center">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome reperto</th>
+                    <th scope="col">Sezione</th>
+                    <th scope="col">Data catalogazione</th>
+                    <th scope="col">Stato</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                
+                include 'connect.php';
+                
+                $nome = $_POST['nome'];
+                $anno_inizio_uso = $_POST['anno_inizio'];
+                $anno_fine_uso = $_POST['anno_fine'];
+                $autore = $_POST['autore'];
+                $sezione = $_POST['sezione'];
+                $stato = $_POST['stato'];
+                $materiale = $_POST['materiale'];
+                $acquisizione = $_POST['acquisizione'];
+                
+                // $id = $row['codassoluto'];
+                // $nome = $row['nome'];
+                // $sezione = $row['sezione'];
+                // $data = $row['datacatalogazione'];
+                // $stato = $row['stato'];
+                
+                $query = 'SELECT DISTINCT repertinuova.codassoluto, repertinuova.nome, repertinuova.sezione, repertinuova.datacatalogazione, repertinuova.stato
+                FROM repertinuova, hafatto, compostoda, acquisizioni, autore
+                WHERE repertinuova.nome LIKE "%'.$nome.'%"
+                AND repertinuova.annoiniziouso LIKE "%'.$anno_inizio_uso.'%"
+                AND repertinuova.annofineuso LIKE "%'.$anno_fine_uso.'%"
+                AND repertinuova.sezione LIKE "%'.$sezione.'%"
+                AND repertinuova.stato LIKE "%'.$stato.'%"
+                AND (hafatto.codassoluto = repertinuova.codassoluto
+                    AND hafatto.codautore LIKE "%'.$autore.'%")
+                AND (compostoda.nomemateriale LIKE "%'.$materiale.'%"
+                    AND compostoda.codassoluto = repertinuova.codassoluto)
+                AND (acquisizioni.tipoacquisizione LIKE "%'.$acquisizione.'%"
+                    AND acquisizioni.codassoluto = repertinuova.codassoluto)';
+                
+                $result = mysqli_query($conn, $query) or die("Error");
+                
+                if($result) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['codassoluto'];
+                        $nome = $row['nome'];
+                        $sezione = $row['sezione'];
+                        $data = $row['datacatalogazione'];
+                        $stato = $row['stato'];
+
+                        switch ($sezione) {
+                            case "E":
+                                $sezione = "Elettronica";
+                                break;
+                            case "I":
+                                $sezione = "Informatica";
+                                break;
+                            case "M":
+                                $sezione = "Meccanica";
+                                break;
+                            case "S":
+                                $sezione = "Scienze";
+                                break;
+                        }
+
+                        echo '<tr>
+                        <th scope="row">' . $id . '</th>
+                        <td>' . $nome . '</td>
+                        <td>' . $sezione . '</td>
+                        <td>' . $data . '</td>
+                        <td>' . $stato . '</td>
+                        <td>
+                            <button class="btn btn-primary"><a href="modifica-reperto.php?id=' . $id . ' " class="text-light text-decoration-none">Modifica</a></button>
+                            <button class="btn btn-danger"><a href="cancella-reperto.php?id=' . $id . ' " class="text-light text-decoration-none">Cancella</a></button>
+                        </td>
+                        </tr>';
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+    
