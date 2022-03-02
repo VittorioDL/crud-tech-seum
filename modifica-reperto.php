cb0a0c403a45b5e86b1a7b38d85346d7ef9c7ec4
simonapazienza <100277357+simonapazienza@users.noreply.github.com>
2022-03-02 13:36:02 +0100

    modifica pulsante

--- modifica-reperto.php
+++ modifica-reperto.php
@@ -8,16 +8,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <!-- Pulsante per tornare alla home -->
-    <div class="container"> 
-        <button class="btn btn-primary my-5"> <a href="home.php" class="text-light text-decoration-none">Home</a></button>
+    
+    
+        <div class="container"> 
+        <?php
+            session_start();
+            $permessi = $_SESSION['permessi'];
+
+            if($permessi==0)
+            {
+                echo '<a href="home.php" class="text-light text-decoration-none btn btn-primary my-5">Home</a>';
+            }
+            else if($permessi==1)
+            {
+                echo '<a href="home-admin.php" class="text-light text-decoration-none btn btn-primary my-5">Home</a>';
+            }
+        ?>
     </div>
-    <?php
-        include 'connect.php';
-
         
-
-    ?>
 
     <!-- Pulsante per modificare il database -->
     <div class="container"> 
