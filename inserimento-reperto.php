461314963cfdc82fab5f4b8a71f10b9a08eda9a5
simonapazienza <100277357+simonapazienza@users.noreply.github.com>
2022-03-02 14:45:22 +0100

    Inserimento modifica terminato

--- inserimento-reperto.php
+++ inserimento-reperto.php
@@ -6,8 +6,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-    
+<form action='inserimento-database.php' method='post'>
+
+    <input name="nome" type="text" class="form-control" placeholder="Nome reperto"><br><br>
+    <input name="denominazione_storica" type="text" class="form-control" placeholder="Denominazione storica"><br><br>
+    <input name="immagine" type="file" class="form-control" placeholder="Seleziona file"><br><br>
+    <input name="data_catalogazione" type="date" class="form-control" placeholder="Data catalogazione"><br><br>
+    <input name="anno_inizio" type="number" class="form-control" min="1800" step="1" placeholder="Anno inizio uso"><br><br>
+    <input name="anno_fine" type="number" class="form-control" min="1800" step="1" placeholder="Anno fine uso"><br><br>  
+
+    <!-- Selezione autore -->
+    <select name="autore" class="form-control">
+        <option value="" class="text-white bg-secondary">Autore</option>
+            <?php
+                $queryAutori = "select * from autore order by nomeautore asc";
+                $result1 = mysqli_query($conn, $queryAutori);
+                while ($row = mysqli_fetch_array($result1)) {
+                    echo '<option value=' . $row['codautore'] . '>' . $row['nomeautore'] . '</option>';
+                }
+            ?>
+    </select><br><br>
+
+    <!-- Selezione sezione -->
+    <select name="sezione" class="form-control">
+        <option value="" class="text-white bg-secondary">Sezione</option>
+        <option value="I">Informatica</option>
+        <option value="E">Elettronica</option>
+        <option value="M">Meccanica</option>
+        <option value="S">Scienze</option>
+    </select><br><br>
+
+    <!-- Selezione stato -->
+    <select name="stato" class="form-control">
+        <option value="" class="text-white bg-secondary">Stato</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select><br><br>
+
+    <!-- Selezione materiale -->
+    <select name="materiale" class="form-control">
+    <option value="" class="text-white bg-secondary">Materiale</option>
+        <?php
+            $queryMateriale = "select nomemateriale from compostoda";
+            $result2 = mysqli_query($conn, $queryMateriale);
+            $materiali = array();
+            while ($row = mysqli_fetch_array($result2)) {
+                array_push($materiali, $row['nomemateriale']);
+            }
+            $materiali_unici = array_unique($materiali);
+            foreach ($materiali_unici as $m) {
+                echo '<option value=' . $m . '>' . $m . '</option>';
+            }
+        ?>
+    </select><br><br>
+
+    <!-- Selezione acquisizione -->
+    <select name="acquisizione" class="form-control">
+        <option value="" class="text-white bg-secondary">Acquisizione</option>
+        <option value="D">Donazione</option>
+        <option value="A">Acquisto</option>
+        <option value="R">Rubato</option>
+        <option value="T">Trovato</option>
+        <option value="C">Costruito</option>
+        <option value="O">Altro tipo</option>
+    </select><br><br>
+
+    <textarea name="definizione" type="text" class="form-control" placeholder="Definizione reperto" rows="3"></textarea><br><br>
+    <textarea name="descrizione" type="text" class="form-control" placeholder="Descrizione reperto" rows="3"></textarea><br><br>
+    <textarea name="modo_uso" type="text" class="form-control" placeholder="Modo d'uso" rows="3"></textarea><br><br>
+    <textarea name="scopo" type="text" class="form-control" placeholder="Scopo del reperto" rows="3"></textarea><br><br>
+    <textarea name="osservazioni" type="text" class="form-control" placeholder="Osservazioni reperto" rows="3"></textarea><br><br>
+   
+
+</form>
+    <div class="container"> 
+        <button class="btn btn-primary my-5"> <a href="inserimento-database.php" class="text-light text-decoration-none">Aggiungi</a></button>
+    </div>
 </body>
 </html>
