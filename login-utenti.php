ef168c6ddeffa812a44114608ac0a4fed48e3b32
BigG17 <alessandrocrisetti117@gmail.com>
2022-03-02 13:02:07 +0100

    Create login-utenti.php
    Inserimento credenziali degli utenti

---
+++ login-utenti.php
@@ -0,0 +1,25 @@
+<?php session_destroy();?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <form  method = 'post' action = 'check_login.php'>
+        <center> LOGIN<br><br></center>
+
+        <!--input dell'utente -->
+        <center><input name = 'utente' type = 'text' size = '25' maxlength = '21'  placeholder = "Username"></center> <br><br>
+
+        <!--input della password -->
+        <center><input name = 'password' type = 'password' size = '25' maxlength = '16' placeholder = "Password"></center> <br><br>
+
+        <center><button type = 'submit'> SUBMIT </button></center> <br><br><!--bottone per l'accesso -->
+    </form>
+</body>
+</html>
