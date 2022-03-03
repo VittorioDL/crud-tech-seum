9a9b405527d8c5611b956a2c215aa78805972003
simonapazienza <100277357+simonapazienza@users.noreply.github.com>
2022-03-03 13:39:39 +0100

    modifica inserimento

--- inserimento-reperto.php
+++ inserimento-reperto.php
@@ -16,7 +16,7 @@ include 'connect.php';
 ?>
 
 <body>
-    <div class="container"> 
+    <div class="container bg-light p-2  bg-opacity-75"> 
             <?php
                 session_start();
                 $permessi = $_SESSION['permessi'];
@@ -32,7 +32,7 @@ include 'connect.php';
             ?>
     </div>
 
-    <div class="container py-1">
+    <div class="container py-1 bg-light p-2 text-black bg-opacity-75">
         <div class="d-flex justify-content-between">
             <h2 class="mx-auto">Aggiungi il reperto</h2>
         </div>
@@ -57,101 +57,100 @@ include 'connect.php';
                     <li>
                         <input name="data_catalogazione" type="date" class="col-auto my-2" placeholder="Data catalogazione"><br><br>
                     </li>
-		    <li>
-		    	<input name="anno_inizio" type="number" class="col-auto my-2" min="1800" step="1" placeholder="Anno inizio uso"><br><br>
-		    </li>
-	            <li>
-			<input name="anno_fine" type="number" class="col-auto my-2" min="1800" step="1" placeholder="Anno fine uso"><br><br> 
-	            </li>
+		            <li>
+		    	        <input name="anno_inizio" type="number" class="col-auto my-2" min="1800" step="1" placeholder="Anno inizio uso"><br><br>
+		            </li>
+	                <li>
+			            <input name="anno_fine" type="number" class="col-auto my-2" min="1800" step="1" placeholder="Anno fine uso"><br><br> 
+	                </li>
+                    <li>
+                        <!-- Selezione stato -->
+                        <p><select name="stato" class="col-auto my-2">
+                        <option value="" class="text-white bg-secondary">Stato</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        </select><br><br></p>
+                    </li>
+                    <li>
+			            <!-- Selezione sezione -->
+                        <p><select name="sezione" class="col-auto my-2">
+                        <option value="" class="text-white bg-secondary">Sezione</option>
+                        <option value="I">Informatica</option>
+                        <option value="E">Elettronica</option>
+                        <option value="M">Meccanica</option>
+                        <option value="S">Scienze</option>
+                        </select><br><br></p>                        
+                    </li>
+                    <li>
+			            <!-- Selezione acquisizione -->
+                        <p><select name="acquisizione" class="col-auto my-2">
+                        <option value="" class="text-white bg-secondary">Acquisizione</option>
+                        <option value="D">Donazione</option>
+                        <option value="A">Acquisto</option>
+                        <option value="R">Rubato</option>
+                        <option value="T">Trovato</option>
+                        <option value="C">Costruito</option>
+                        <option value="O">Altro tipo</option>
+                        </select><br><br></p>    
+                    </li>
+                    <li>
+			            <!-- Selezione materiale -->
+    			        <p><select name="materiale" class="col-auto my-2">
+    			        <option value="" class="text-white bg-secondary">Materiale</option>
+        		        <?php
+           		            $queryMateriale = "select nomemateriale from compostoda";
+            		        $result2 = mysqli_query($conn, $queryMateriale);
+            		        $materiali = array();
+           		            while ($row = mysqli_fetch_array($result2)) {
+                                array_push($materiali, $row['nomemateriale']);
+           		            }
+           		            $materiali_unici = array_unique($materiali);
+            		        foreach ($materiali_unici as $m) {
+               		            echo '<option value=' . $m . '>' . $m . '</option>';
+            		        }
+        		        ?>
+    			        </select><br><br></p>  
+                    </li>
+		            <li>
+			            <!-- Selezione autore -->
+                        <p><select name="autore" class="col-auto my-2">
+                        <option value="" class="text-white bg-secondary">Autore</option>
+                        <?php
+                            $queryAutori = "select * from autore order by nomeautore asc";
+                            $result1 = mysqli_query($conn, $queryAutori);
+                            while ($row = mysqli_fetch_array($result1)) {
+                                echo '<option value=' . $row['codautore'] . '>' . $row['nomeautore'] . '</option>';
+                            }
+            		    ?>
+    			        </select><br><br></p>
+                    </li>
                 </ul>
             </div>
             <div class="col-lg-8 bg-light p-2 text-white bg-opacity-75">
-		<ul>
-		    <li>
-                        <!-- Selezione stato -->
-    			<p><select name="stato" class="col-auto my-2">
-        		<option value="" class="text-white bg-secondary">Stato</option>
-        		<option value="1">1</option>
-        		<option value="2">2</option>
-        		<option value="3">3</option>
-        		<option value="4">4</option>
-        		<option value="5">5</option>
-    			</select><br><br></p>
+		        <ul>
+                    <li>
+                        <textarea name="definizione" type="text" class="col-auto my-2" placeholder="Definizione reperto" rows="3"></textarea><br><br>
                     </li>
                     <li>
-			<!-- Selezione sezione -->
-    			<p><select name="sezione" class="col-auto my-2">
-       			<option value="" class="text-white bg-secondary">Sezione</option>
-        		<option value="I">Informatica</option>
-        		<option value="E">Elettronica</option>
-        		<option value="M">Meccanica</option>
-        		<option value="S">Scienze</option>
-    			</select><br><br></p>
-                        
+                        <textarea name="descrizione" type="text" class="col-auto my-2" placeholder="Descrizione reperto" rows="3"></textarea><br><br>
                     </li>
                     <li>
-			<!-- Selezione acquisizione -->
-    			<p><select name="acquisizione" class="col-auto my-2">
-        		<option value="" class="text-white bg-secondary">Acquisizione</option>
-        		<option value="D">Donazione</option>
-        		<option value="A">Acquisto</option>
-        		<option value="R">Rubato</option>
-        		<option value="T">Trovato</option>
-        		<option value="C">Costruito</option>
-       			<option value="O">Altro tipo</option>
-    			</select><br><br></p>
-                        
+                        <textarea name="modo_uso" type="text" class="col-auto my-2" placeholder="Modo d'uso" rows="3"></textarea><br><br>
                     </li>
                     <li>
-			<!-- Selezione materiale -->
-    			<p><select name="materiale" class="col-auto my-2">
-    			<option value="" class="text-white bg-secondary">Materiale</option>
-        		<?php
-           		 $queryMateriale = "select nomemateriale from compostoda";
-            		 $result2 = mysqli_query($conn, $queryMateriale);
-            		 $materiali = array();
-           		 while ($row = mysqli_fetch_array($result2)) {
-                         array_push($materiali, $row['nomemateriale']);
-           		 }
-           		 $materiali_unici = array_unique($materiali);
-            		foreach ($materiali_unici as $m) {
-               		 echo '<option value=' . $m . '>' . $m . '</option>';
-            		}
-        		?>
-    			</select><br><br></p>
+                        <textarea name="scopo" type="text" class="col-auto my-2" placeholder="Scopo del reperto" rows="3"></textarea><br><br>
+                    </li>
+                    <li>
+                        <textarea name="osservazioni" type="text" class="col-auto my-2" placeholder="Osservazioni reperto" rows="3"></textarea><br><br>
+                    </li>
 	
-                        
-                    </li>
-		    <li>
-			<!-- Selezione autore -->
-    			<p><select name="autore" class="col-auto my-2">
-        		<option value="" class="text-white bg-secondary">Autore</option>
-           		 <?php
-                		$queryAutori = "select * from autore order by nomeautore asc";
-                		$result1 = mysqli_query($conn, $queryAutori);
-                		while ($row = mysqli_fetch_array($result1)) {
-                    		echo '<option value=' . $row['codautore'] . '>' . $row['nomeautore'] . '</option>';
-                		}
-            		?>
-    			</select><br><br></p>
-		    	
-		    </li>
-	
-		</ul>
+		        </ul>
             </div>
         </div>
-    </div>
-
-    <div class="container my-5">
-        <div class="input-group mb-10">
-            <textarea name="definizione" type="text" class="col-auto my-2" placeholder="Definizione reperto" rows="3"></textarea><br><br>
-            <textarea name="descrizione" type="text" class="col-auto my-2" placeholder="Descrizione reperto" rows="3"></textarea><br><br>
-            <textarea name="modo_uso" type="text" class="col-auto my-2" placeholder="Modo d'uso" rows="3"></textarea><br><br>
-            <textarea name="scopo" type="text" class="col-auto my-2" placeholder="Scopo del reperto" rows="3"></textarea><br><br>
-            <textarea name="osservazioni" type="text" class="col-auto my-2" placeholder="Osservazioni reperto" rows="3"></textarea><br><br>
-        </div>
-    </div>
-
+    </div> 
 </form>
     <div class="container"> 
     <p class="text-center"><button class="btn btn-success"> <a href="inserimento-database.php" class="text-light text-decoration-none">Aggiungi</a></button></p>
