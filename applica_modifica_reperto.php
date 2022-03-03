3e9feb879bd102022fc0b715a903656a46807e32
Nilo Netti <nilo.netti2003@gmail.com>
2022-03-03 08:40:12 +0100

    eliminazione file superflui

---
+++ applica_modifica_reperto.php
@@ -0,0 +1,34 @@
+<?php
+    include("connessione.php");
+
+    $datacatalogazione = $_GET['datacatalogazione'];
+    $nome = $_GET['nome'];
+    $sezione = $_GET['sezione'];
+    $codrelativo = $_GET['codrelativo'];
+    $definizione = $_GET['definizione'];
+    $denominazionestorica = $_GET['denominazionestorica'];
+    $descrizione = $_GET['descrizione'];
+    $modouso = $_GET['modouso'];
+    $annoiniziouso = $_GET['annoiniziouso'];
+    $annofineuso = $_GET['annofineuso'];
+    $scopo = $_GET['scopo'];
+    $stato = $_GET['stato'];
+    $osservazioni = $_GET['osservazioni'];
+
+    #$permessi = $_SESSION['permessi'];
+    $permessi = 1;
+
+    $query = "INSERT INTO repertinuova ( datacatalogazione, nome, sezione, codrelativo, definizione, denominazionestorica, descrizione, modouso, annoiniziouso, annofineuso, scopo,stato, osservazioni)
+     VALUES ('$datacatalogazione', '$nome', '$sezione', $codrelativo, '$definizione', '$denominazionestorica', '$descrizione', '$modouso', '$annoiniziouso', '$annofineuso', '$scopo', $stato, '$osservazioni')";
+
+
+    echo $query;
+    $result = mysqli_query($conn, $query) or die ("errore");
+    mysqli_close($con);
+
+    if ($permessi == 1)
+        header("location:admin.php");
+    else if($permessi == 0)
+        header("location:home.php");
+
+?>
