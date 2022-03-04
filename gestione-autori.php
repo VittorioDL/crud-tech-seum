75e3e29c2878ebc40a1e37d0a94c64c205f9c541
VittorioDL <victor03.VDL@gmail.com>
2022-03-04 09:23:13 +0100

    Aggiunta pulsante home

--- gestione-autori.php
+++ gestione-autori.php
@@ -28,6 +28,26 @@ if(!isset($_SESSION['logged_in'])){
         </div>
     </div>
 
+    <!-- Pulsante per tornare alla home -->
+    <div class="container"> 
+        <?php
+            if(!isset($_SESSION['logged_in'])){
+                header("Location: login-utenti.php"); 
+            }
+
+            $permessi = $_SESSION['permessi'];
+            
+            if($permessi==0)
+            {
+                echo '<a href="home.php" class="text-light text-decoration-none btn btn-primary my-5">Home</a>';
+            }
+            else if($permessi==1)
+            {
+                echo '<a href="home-admin.php" class="text-light text-decoration-none btn btn-primary my-5">Home</a>';
+            }
+        ?>
+    </div>
+
     <div class="container my-5">
         <div class="d-flex justify-content-between">
             <!-- Bottone aggiunta autori -->
