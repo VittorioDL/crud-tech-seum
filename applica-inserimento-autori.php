74fbff8a6a4ca1680d191c73e25e302689ea676f
xNumaX <81299290+xNumaX@users.noreply.github.com>
2022-03-05 08:44:57 +0100

    Update applica-inserimento-autori.php

--- applica-inserimento-autori.php
+++ applica-inserimento-autori.php
@@ -1,5 +1,5 @@
 <?php
-  include 'connect.php';
+  include 'connessione.php';
   session_start();
 
   $permission=$_SESSION['permessi'];
@@ -8,8 +8,8 @@
   $inizio=$_POST['anno_n'];
   $fine=$_POST['anno_f'];
 
-  $query="INSERT INTO autore(nomeautore, annonascita, annofine) VALUES ('$nome_a','$anno_n','$anno_f')";
-  $res=mysqli_query($con,$query);
+  $query="INSERT INTO autore(nomeautore, annonascita, annofine) VALUES ('$nome','$inizio','$fine')";
+  $res=mysqli_query($conn,$query);
 
   if ($permission==0)
     header("location: home.php");
