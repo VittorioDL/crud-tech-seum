da7c9c73bd3998fa66ff8d47ef64bcf81834ec9e
xNumaX <81299290+xNumaX@users.noreply.github.com>
2022-03-05 08:46:01 +0100

    Update applica-modifica-autori.php

--- applica-modifica-autori.php
+++ applica-modifica-autori.php
@@ -1,16 +1,16 @@
 <?php
-  include 'connect.php';
+  include 'connessione.php';
   session_start();
 
   $permission=$_SESSION['permessi'];
-
-  $codice=$_POST['cod_a'];
+  $codice=$_SESSION['cod_a'];
+  
   $nome=$_POST['nome_a'];
   $inizio=$_POST['anno_n'];
   $fine=$_POST['anno_f'];
 
   $query="UPDATE autori SET codautore='$codice', nomeautore='$nome', annonascita='$inizio', annofine='$fine' WHERE codautore='$codice';";
-  $res=mysqli_query($con,$query);
+  $res=mysqli_query($conn,$query);
 
   if ($permission==0)
     header("location: home.php");
